f436e24b45315936d3b1132c7e94e3f57e8af02c
Aigerim Yerkabayeva <yy.aigerim@gmail.com>
2021-10-26 22:02:38 +0600

    Created new component for filter buttons

---
+++ src/components/switchCategories.vue
@@ -0,0 +1,65 @@
+<template>
+  <form class="switchCategories form" action="" method="POST">
+      <fieldset class="form-block form__fieldset">
+          <ul>
+            <li v-for="(item, index) in categories" :key="index" class="form-block__radios">
+              <input @change="toggleCat(selectedCategory)" :class="item.className"
+                  type="radio"
+                  name="item"
+                  v-model="selectedCategory"
+                  :value="item.value"
+                  :id="item.id"
+                  :checked="item.checked">
+              <label class="form-block__radio" :for="item.id">
+                  <span>{{ item.title }}</span>
+              </label>
+            </li>
+          </ul>
+      </fieldset>
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'v-switchCategories',
+  props: {},
+  data() {
+    return {
+      categories: [
+        {
+          id: 'item1-1',
+          className: 'v-all',
+          checked: true,
+          title: 'Все товары',
+          value: 'all',
+        },
+        {
+          id: 'item2-1',
+          className: 'v-clothes',
+          checked: false,
+          title: 'Одежда',
+          value: 'clothes',
+        },
+        {
+          id: 'item3-1',
+          className: 'v-accessories',
+          checked: false,
+          title: 'Аксессуары',
+          value: 'accessories',
+        },
+      ],
+      selectedCategory: 'all',
+    };
+  },
+  computed: {},
+  methods: {
+    toggleCat(selectedCategory) {
+      this.$emit('choose', selectedCategory);
+    },
+  },
+};
+</script>
+
+<style>
+
+</style>
