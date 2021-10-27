72920684e402a9d308e733fdecd806c0334289bd
Aigerim Yerkabayeva <yy.aigerim@gmail.com>
2021-10-27 14:48:57 +0600

    Created new component for product card

---
+++ src/components/card.vue
@@ -0,0 +1,34 @@
+<template>
+  <li class="products-list__item">
+    <div class="item-photo">
+        <img class="js-card-img"
+            v-bind:src="item.img"
+            width="330" height="330"
+            alt="Фото футболки 'Эволюционируй или сдохни'">
+        <span v-if="item.isNew" class="badge js-badge">
+          new
+        </span>
+    </div>
+    <div class="item-info">
+        <span class="item-info__points js-item-info__points">
+            {{ item.price }} баллов
+        </span>
+        <p class="item-info__title js-item-info__title">
+            {{ item.title }}
+        </p>
+        <span class="item-info__sizes js-item-info__sizes">
+            Размеры {{ item.sizes }}
+        </span>
+    </div>
+    <button class="button products-list__button" type="button">
+        Заказать
+    </button>
+</li>
+</template>
+
+<script>
+export default {
+  name: 'v-card',
+  props: ['item'],
+};
+</script>
