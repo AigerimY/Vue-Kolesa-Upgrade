e9b0c35282312a1a16340aca99a6385c4a2b467a
Aigerim Yerkabayeva <yy.aigerim@gmail.com>
2021-10-28 10:11:06 +0600

    Fetched data from server for user block

--- src/components/user.vue
+++ src/components/user.vue
@@ -1,18 +1,37 @@
 <template>
   <a class="user">
     <img
-        src="../img/avatar.jpg"
+        :src="user.avatarUrl"
         width="44" height="44"
         alt="Фото пользователя">
     <div>
-        <span class="user__name">Мортиджан</span><br>
-        <span class="user__points">300 баллов</span>
+        <span class="user__name">{{ user.name }}</span><br>
+        <span class="user__points">{{ user.score }} баллов</span>
     </div>
   </a>
 </template>
 
 <script>
+import axios from '../axios';
+
+const userAvatar = require('../img/avatar.jpg');
+
 export default {
   name: 'user-block',
+  data() {
+    return {
+      user: {
+        name: 'Морти',
+        score: 200,
+        avatarUrl: userAvatar,
+      },
+    };
+  },
+  mounted() {
+    axios.get('templates/7ZW3y5GAuIge/data')
+      .then((response) => {
+        this.user = response.data;
+      });
+  },
 };
 </script>
