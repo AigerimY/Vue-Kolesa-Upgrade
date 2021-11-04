f4f085290bfa95f7c0ca47694a2739053868dea2
Aigerim Yerkabayeva <yy.aigerim@gmail.com>
2021-11-04 10:25:04 +0600

    Connected store to user block

--- src/layouts/components/user.vue
+++ src/layouts/components/user.vue
@@ -1,36 +1,23 @@
 <template>
   <a class="user">
     <img
-        :src="user.avatarUrl"
+        :src="$store.state.userData.avatarUrl"
         width="44" height="44"
         alt="Фото пользователя"
       >
     <div>
-      <span class="user__name">{{ user.name }}</span><br>
-      <span class="user__points">{{ user.score }} баллов</span>
+      <span class="user__name">{{ $store.state.userData.name }}</span><br>
+      <span class="user__points">{{ $store.state.userData.score }} баллов</span>
     </div>
   </a>
 </template>
 
 <script>
-import axios from '../../axios';
 
 export default {
   name: 'user-block',
-  data() {
-    return {
-      user: Object,
-    };
-  },
   mounted() {
-    axios.get('templates/7ZW3y5GAuIge/data')
-      .then((response) => {
-        this.user = response.data;
-        this.$emit('load', this.user);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
+    this.$store.dispatch('updateUserData');
   },
 };
 </script>
